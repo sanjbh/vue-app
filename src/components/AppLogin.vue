f6f7eabdaeca2df604704c97dd82fe82c50fb274
Sanjay Bhattacharya <sanjbh@gmail.com>
2024-01-21 18:00:17 +0530

    Go backend integration

--- src/components/AppLogin.vue
+++ src/components/AppLogin.vue
@@ -5,18 +5,8 @@
                 <h1 class="mt-5">Login</h1>
                 <hr />
                 <FormTag @myevent="submitHandler" name="MyForm" event="myevent">
-                    <TextInput 
-                        v-model="email" 
-                        label="Email" 
-                        type="email" 
-                        name="email" 
-                        required="true" />
-                    <TextInput 
-                        v-model="password" 
-                        label="Password" 
-                        type="password" 
-                        name="password" 
-                        required="true" />
+                    <TextInput v-model="email" label="Email" type="email" name="email" required="true" />
+                    <TextInput v-model="password" label="Password" type="password" name="password" required="true" />
                     <hr />
                     <input class="btn btn-primary" type="submit" value="Login">
                 </FormTag>
@@ -44,6 +34,23 @@ export default {
     methods: {
         submitHandler() {
             console.log("submitHanlder called: success");
+            const payload = {
+                email: this.email,
+                password: this.password
+            }
+            const requestOptions = {
+                method: 'POST',
+                body: JSON.stringify(payload)
+            }
+            fetch('http://localhost:8081/users/login', requestOptions)
+                .then(response => response.json())
+                .then((data) => {
+                    if (data.error)
+                        console.log('Error: ', data.error);
+                    else {
+                        console.log(data)
+                    }
+                })
         }
     }
 }
